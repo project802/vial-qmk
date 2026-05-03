32d31448ffff8959e2ff24a1bc07dbae05969738
Chris Vincent <project802@users.noreply.github.com>
2026-05-03 18:34:21 -0400

    Legibility improvement
    Add more comments and clean up some code to make it easier to read. No functional change.

--- keyboards/ydkb/kunlun/matrix.c
+++ keyboards/ydkb/kunlun/matrix.c
@@ -17,7 +17,7 @@ typedef enum {
     KUNLUN_PCB_RIGHT = 1,
 } kunlun_pcb_t;
 
-static uint8_t scan_get_key_pressed( kunlun_pcb_t pcb );
+static bool scan_is_key_pressed( kunlun_pcb_t pcb );
 static void scan_setup_first_col( void );
 static void scan_setup_next_col( void );
 
@@ -79,39 +79,53 @@ void matrix_init(void)
 /*
  * Scans the matrix and updates the state.
  *
- * - Shift in a single 0 bit to the first register on both left and right PCBs
- * - Iterate the columns within the rows in a loop:
- *   - Even (and zero) columns read the selected key on the left PCB (PB3 low = pressed)
- *   - Odd columns read the selected key on the right PCB (PB2 low = pressed) and pulse the shift clock
+ * The keyboard hardware is actually 40 rows (max number of outputs in the shift
+ * register serial arrangement) and 2 columns (each PCB), but QMK doesn't handle
+ * 40 rows efficiently.
+ * 
+ * The original designers seem to have decided to use the concept of "real column"
+ * to translate the QMK logical matrix to the hardware matrix.
+ *
+ * - Shift in a single 0 bit to the first register output on both left and 
+ *   right PCBs. This sets up the first hardware row for evaluation.
+ * - Iterate the logical columns within the logical rows in a loop:
+ *   - Even (and zero) logical columns read the left PCB (PB3 low = pressed)
+ *   - Odd logical columns read the right PCB (PB2 low = pressed) and pulse
+ *     the shift clock for next hardware row
  */
 uint8_t matrix_scan(void)
 {
     static matrix_row_t matrix_raw[MATRIX_ROWS] = {0};
-    
-    static uint16_t matrix_scan_timestamp = 0;
+    static uint16_t     matrix_scan_timestamp   = 0;
 
-    bool changed = false;
-
+    bool     changed    = false;
     uint16_t time_check = timer_read();
-    if (matrix_scan_timestamp == time_check) return 1;
+
+    // Avoid scanning the matrix faster than 1 kHz to give time for other tasks
+    if (matrix_scan_timestamp == time_check) return 0;
     matrix_scan_timestamp = time_check;
 
     scan_setup_first_col();
 
-    for (uint8_t row=0; row<MATRIX_ROWS; row++) {
+    for (uint8_t row=0; row<MATRIX_ROWS; row++)
+    {
         matrix_row_t row_raw = 0;
 
-        for (uint8_t col=0; col<MATRIX_COLS; col++) {
+        for (uint8_t col=0; col<MATRIX_COLS; col++)
+        {
             uint8_t real_col = col/2;
             if (col & 1) real_col += 8;
 
-            if( scan_get_key_pressed(real_col < 8 ? KUNLUN_PCB_LEFT : KUNLUN_PCB_RIGHT) )
+            kunlun_pcb_t pcb = real_col < 8 ? KUNLUN_PCB_LEFT : KUNLUN_PCB_RIGHT;
+            if( scan_is_key_pressed(pcb) )
             {
                 row_raw |= (1 << real_col);
             }
 
             if (real_col >= 8)
+            {
                 scan_setup_next_col();
+            }
         }
 
         if( matrix_raw[row] != row_raw )
@@ -135,8 +149,12 @@ uint8_t matrix_scan(void)
 
 /*
  * Read the current key state depending on which PCB is selected.
+ *
+ * The shift register drives low on the hardware column under test so pressing 
+ * the corresponding switch will pull either the left PCB input (PB3) or the 
+ * right PCB input (PB2) low.
  */
-static uint8_t scan_get_key_pressed(kunlun_pcb_t pcb) {
+static bool scan_is_key_pressed(kunlun_pcb_t pcb) {
     if (pcb == KUNLUN_PCB_LEFT)
     {
         return (PINB & (1<<PB3)) == 0;
@@ -148,15 +166,16 @@ static uint8_t scan_get_key_pressed(kunlun_pcb_t pcb) {
 }
 
 /*
- * Clock in the keypress test bit (0), which will then get shifted through the registers
- * used to test the keys one-by-one.
+ * Clock in the keypress test bit (0), which will then get shifted through the 
+ * registers used to test the keys one-by-one.
  */
 static void scan_setup_first_col( void )
 {
     // PB3 | serial data = output, low
     //
-    // Setup time is not specified in the data sheet, but with a max clock of 30 MHz
-    // (cycle time of 33 ns) and a single nop is about 150 us, this should have enough margin.
+    // Setup time is not specified in the data sheet, but with a max clock of 
+    // 30 MHz (cycle time of 33 ns) and a single nop is about 150 us, this should 
+    // have enough margin.
     DDRB |= (1<<PB3);
     PORTB &= ~(1<<PB3);
     asm("nop");
@@ -166,8 +185,8 @@ static void scan_setup_first_col( void )
     SERIAL_DATA_CLOCK_PULSE();
 
     // PB3 = high
-    // This returns to high faster than flipping straight to input with a weak pull-up
-    // so we can get to scanning much sooner.
+    // This returns to high faster than flipping straight to input with a weak 
+    // pull-up so we can get to scanning much sooner.
     PORTB |= (1<<PB3);
 
     // PB3 = input (and pull-up enabled by previous line)
@@ -176,7 +195,8 @@ static void scan_setup_first_col( void )
 }
 
 /*
- * Clock in a 1 bit, which will continue to shift the keypress test bit through the registers.
+ * Clock in a 1 bit, which will continue to shift the keypress test bit through
+ * the registers.
  */
 static void scan_setup_next_col( void )
 {
