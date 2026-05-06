e01f076bdfcdfc1955a9fc719d8faf14db186f55
Chris Vincent <project802@users.noreply.github.com>
2026-05-06 19:45:31 -0400

    Horse trade a few features
    Get key override in exchange for one shot and magic.

--- keyboards/ydkb/kunlun/config.h
+++ keyboards/ydkb/kunlun/config.h
@@ -4,24 +4,7 @@
 #define MATRIX_ROWS 5
 #define MATRIX_COLS 16
 
-/* fix space cadet rollover issue */
-#define DISABLE_SPACE_CADET_ROLLOVER
-
 /*
- * Feature disable options
- *  These options are also useful to firmware size reduction.
+ * Feature disable options to reduce firmware size
  */
-
-/* disable debug print */
-// #define NO_DEBUG
-
-/* disable print */
-// #define NO_PRINT
-
-/* disable action features */
-//#define NO_ACTION_LAYER
-//#define NO_ACTION_TAPPING
-//#define NO_ACTION_ONESHOT
-//#define NO_ACTION_MACRO
-//#define NO_ACTION_FUNCTION
-//#define DEBUG_MATRIX_SCAN_RATE
+#define NO_ACTION_ONESHOT
